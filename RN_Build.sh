f16b47e00a92e7381aeff3be4c543f9d938ec55f
Mohamed Abdou <abdou@snoonu.com>
2020-10-19 07:40:26 +0300

    Added CD to AppCenter

--- RN_Build.sh
+++ RN_Build.sh
@@ -4,6 +4,14 @@ BUILDS_FOLDER=Builds
 rm -rf $BUILDS_FOLDER
 mkdir $BUILDS_FOLDER
 
+APPCENTER_USERNAME=<ORG_USER_NAME_HERE>
+IOS_APPCENTER_IDENTIFIER=<IOS_APPCENTER_IDENTIFIER_HERE>
+ANDROID_APPCENTER_IDENTIFIER=<ANDROID_APPCENTER_IDENTIFIER_HERE>
+
+APP_CENTER_TOKEN_IOS=<IOS_TOKEN_APPCENTER_HERE>
+APP_CENTER_TOKEN_ANDROID=<ANDROID_TOKEN_APPCENTER_HERE>
+
+############### BUILDING/Integration ###############
 build_ios(){
 	EXPORT_FILE=exportOptions.plist
 	DEPLOYMENT_POSTPROCESSING=YES
@@ -20,7 +28,7 @@ build_ios(){
 	echo '<plist version="1.0">' >> $EXPORT_FILE
 	echo '<dict>' >> $EXPORT_FILE
 	echo '    <key>method</key>' >> $EXPORT_FILE
-	echo '    <string>app-store</string>' >> $EXPORT_FILE
+	echo '    <string>development</string>' >> $EXPORT_FILE
 	echo '    <key>orxynet</key>' >> $EXPORT_FILE
 	echo '    <string>9U9YQN5235</string>' >> $EXPORT_FILE
 	echo '</dict>' >> $EXPORT_FILE
@@ -52,6 +60,52 @@ build_android(){
 	echo "Android: APK Generated ✅"
 }
 
+############### DEPLYOMENT ###############
+publish_ios(){
+	# Publishing IPA to AppCenter
+	ipaPath=$(find ~/ipas -name "*.ipa" | head -1)
+	echo "Found ipa at $ipaPath"
+
+	if [[ -z ${ipaPath} ]]
+	then
+	    echo "No IPAs were found, skip publishing to App Center 💥"
+	else
+	    echo "Publishing IPA to App Center 🚀"
+	    appcenter distribute release \
+	        --group Collaborators \
+	        --file "${ipaPath}" \
+	        --release-notes 'App submission via Codemagic' \
+	        --app $APPCENTER_USERNAME/$IOS_APPCENTER_IDENTIFIER \
+	        --token "${APP_CENTER_TOKEN_IOS}" \
+	        --quiet
+	fi
+}
+
+publish_android(){
+	# Publishing APK to AppCenter
+	apkPath=$(find build -name "*.apk" | head -1)
+	echo "Found apk at $apkPath"
+	
+	if [[ -z ${apkPath} ]]
+	then
+	    echo "No APKs were found, skip publishing to App Center 💥"
+	else
+	    echo "Publishing APK to App Center 🚀"
+	    appcenter distribute release \
+	        --group Collaborators \
+	        --file "${apkPath}" \
+	        --release-notes 'App submission via Codemagic' \
+	        --app $APPCENTER_USERNAME/$ANDROID_APPCENTER_IDENTIFIER \
+	        --token "${APP_CENTER_TOKEN_ANDROID}" \
+	        --quiet
+	fi
+}
+
+
 build_android &
-# build_ios &
+build_ios &
+wait
+
+publish_ios &
+publish_android &
 wait
